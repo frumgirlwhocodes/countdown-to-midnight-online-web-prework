55d543964e2badcb72f4d44d6190778bb8225a08
lieby weintraub <liebahinda@gmail.com>
2019-02-17 20:55:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,13 +9,10 @@ end
   puts "HAPPY NEW YEAR!"
 end
 def countdown_with_sleep(int)
-  sleep(1)
-  int=10 
-  while int > 0 
-  puts "#{int} SECOND(S)!"
-  int += 1
-end 
-  puts "HAPPY NEW YEAR!"
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(1)
+  end
 end
-  
   
